a0d1b00e9888235254a3e9f2eb0688038357f525
ennyongit <bondarenkoenny@gmail.com>
2025-07-08 18:59:49 +0400

    install fonts; styled topHeader

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import TopHeader from "./components/TopHeader";
-
+import Header from "./components/Header";
 
 const App = () => {
     return (
         <div>
             <TopHeader/>
+            <Header/>
         </div>
     )
 }
--- components/TopHeader.js
+++ components/TopHeader.js
@@ -1,20 +1,59 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebookF,
+  faInstagram,
+  faTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
 const TopHeader = () => {
     return(
-        <div className="bg-black text-white">
-          <nav className="navbar navbar-expand-lg bg-dark navbar-light d-none d-lg-block" id="templatemo_nav_top">
-            <div className="container text-light">
-                <div className="w-100 d-flex justify-content-between">
-                    <div>
-                        <i className="fa fa-envelope mx-2"></i>
-                        <a className="navbar-sm-brand text-light text-decoration-none" href="mailto:info@company.com">info@company.com</a>
-                        <i className="fa fa-phone mx-2"></i>
-                        <a className="navbar-sm-brand text-light text-decoration-none" href="tel:010-020-0340">010-020-0340</a>
+        <div className="bg-gray-800 text-white">
+          <nav className="" id="templatemo_nav_top flex">
+            <div className="text-light w-full">
+                <div className="flex justify-between pt-2 pb-2 pl-3 pr-3">
+                    <div className="flex gap-2">
+                        <div className="flex justify-center items-center gap-2">
+                        <FontAwesomeIcon icon={faEnvelope} style={{width: '11.81px', color: '#cfd6e1'}}/>
+                        <a 
+                            className="text-sm text-gray-300" 
+                            href="mailto:info@company.com"
+                        >
+                            info@company.com</a>
+                        </div>
+
+                        <div className="flex justify-center items-center gap-2">
+                        <FontAwesomeIcon icon={faPhone} style={{width: '11.81px', color: '#cfd6e1'}}/>
+                            <a 
+                            className="text-sm text-gray-300" 
+                            href="tel:010-020-0340"
+                            >
+                              010-020-0340</a>
+                        </div>
+                    
                     </div>
-                    <div>
-                        <a className="text-light" href="https://fb.com/templatemo" target="_blank" rel="sponsored"><i className="fab fa-facebook-f fa-sm fa-fw me-2"></i></a>
-                        <a className="text-light" href="https://www.instagram.com/" target="_blank"><i className="fab fa-instagram fa-sm fa-fw me-2"></i></a>
-                        <a className="text-light" href="https://twitter.com/" target="_blank"><i className="fab fa-twitter fa-sm fa-fw me-2"></i></a>
-                        <a className="text-light" href="https://www.linkedin.com/" target="_blank"><i className="fab fa-linkedin fa-sm fa-fw"></i></a>
+                    <div className="flex gap-3">
+                        <a className="text-light" 
+                            href="https://fb.com/templatemo" target="_blank" rel="sponsored">
+                            <i className="fab fa-facebook-f"></i>
+                                <FontAwesomeIcon icon={faFacebookF} style={{color: '#cfd6e1', width: '7.65px'}}/>
+                            </a>
+                        <a className="text-light" 
+                            href="https://www.instagram.com/" target="_blank">
+                                <i className="fab fa-instagram"></i>
+                                <FontAwesomeIcon icon={faInstagram}  style={{color: '#cfd6e1' , width:'11.28px'}}/>
+                                </a>
+                        <a className="text-light" 
+                            href="https://twitter.com/" target="_blank">
+                            <i className="fab fa-twitter"></i>
+                                <FontAwesomeIcon icon={faTwitter}  style={{color: '#cfd6e1', width:'11.28px'}}/>
+                            </a>
+                        <a className="text-light" 
+                            href="https://www.linkedin.com/" target="_blank">
+                                <i className="fab fa-linkedin"></i>
+                                <FontAwesomeIcon icon={faLinkedin}  style={{color: '#cfd6e1', width:'11.28px'}}/>
+                                </a>
                     </div>
                 </div>
             </div>
